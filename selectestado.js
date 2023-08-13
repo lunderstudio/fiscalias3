10b1469a7420cc8f41a3c998f556ce495be52cba
Armando del Angel <113638993+aarmando2@users.noreply.github.com>
2023-08-12 20:06:50 -0600

    Selector JQery

--- selectestado.js
+++ selectestado.js
@@ -19,26 +19,27 @@ function añoSeleccionado(id_año){
 }
 
 function select_estado(idEstado) {
+  console.log(idEstado);
   var info_estado = this.estado_lista.find(x => x.Entidad === idEstado)
 
-  $("idEntidad").text(info_estado.Entidad);
-  $("IdDenuncias").text(info_estado.Denuncias);
-  $("idInvestigaciones").text(info_estado.Investigaciones);
-  $("idCarpetas").text(info_estado.Carpetas);
-  $("idImputaciones").text(info_estado.Imputaciones);
-  $("idVinculaciones").text(info_estado.Vinculaciones);
-  $("idSentencias").text(info_estado.Sentencias);
-  $("idTotalCombate").text(info_estado.TotalCambate);
-  $("idJudicialización").text(info_estado.Judicialización);
-  $("idCriterios").text(info_estado.Criterio);
-  $("idControversias").text(info_estado.Controversias);
-  $("idSuspencion").text(info_estado.Suspensiónes);
-  $("idProcedimiento").text(info_estado.Procedimiento);
-  $("idReparacion").text(info_estado.Reparaciónes);
-  $("idTotalNoCombate").text(info_estado.TotalNoCombate);
-  $("idArchivo").text(info_estado.Archivos);
-  $("idAcccionPenal").text(info_estado.NoAcciónPenal);
-  $("idAbstenciones").text(info_estado.Abstenciones);
-  $("idPresupuesto").text(info_estado.Presupuesto);
-  $("idMonto").text(info_estado.Montos);
+  $("#idEntidad").text(info_estado.Entidad);
+  $("#IdDenuncias").text(info_estado.Denuncias);
+  $("#idInvestigaciones").text(info_estado.Investigaciones);
+  $("#idCarpetas").text(info_estado.Carpetas);
+  $("#idImputaciones").text(info_estado.Imputaciones);
+  $("#idVinculaciones").text(info_estado.Vinculaciones);
+  $("#idSentencias").text(info_estado.Sentencias);
+  $("#idTotalCombate").text(info_estado.TotalCambate);
+  $("#idJudicialización").text(info_estado.Judicialización);
+  $("#idCriterios").text(info_estado.Criterio);
+  $("#idControversias").text(info_estado.Controversias);
+  $("#idSuspencion").text(info_estado.Suspensiónes);
+  $("#idProcedimiento").text(info_estado.Procedimiento);
+  $("#idReparacion").text(info_estado.Reparaciónes);
+  $("#idTotalNoCombate").text(info_estado.TotalNoCombate);
+  $("#idArchivo").text(info_estado.Archivos);
+  $("#idAcccionPenal").text(info_estado.NoAcciónPenal);
+  $("#idAbstenciones").text(info_estado.Abstenciones);
+  $("#idPresupuesto").text(info_estado.Presupuesto);
+  $("#idMonto").text(info_estado.Montos);
 }
