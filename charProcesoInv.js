93eb0b17dcf676167480ad53d97e9a60789a782a
Armando del Angel <113638993+aarmando2@users.noreply.github.com>
2023-08-15 14:06:29 -0600

    Config grafica de procesos de inv.

--- charProcesoInv.js
+++ charProcesoInv.js
@@ -3,12 +3,12 @@ let myLineChart;
 function chartProcesoInv() {
     // LABELS
     var _entidad = info_estado.Entidad;
-    var _denuncias = 'Denuncias recibidas';
-    var _investigaciones = 'Investigaciones de oficio inicadas';
-    var _carpetas = 'Carpetas de Investigación abiertas';
-    var _imputaciones = 'Formaularios de imputaciones';
-    var _vinculaciones = 'Vinculaciones a proceso';
-    var _sentencias = 'Sentencias condenatorias';
+    var _denuncias = ['Denuncias', 'recibidas'];
+    var _investigaciones = ['Investigaciones de', ' oficio inicadas'];
+    var _carpetas = ['Carpetas de', 'Investigación', 'abiertas'];
+    var _imputaciones = ['Formaularios de', 'imputaciones'];
+    var _vinculaciones = ['Vinculaciones a', 'proceso'];
+    var _sentencias = ['Sentencias', 'condenatorias'];
 
     //Configuracion Char Proceso de Investigacion
     var grapharea = document.getElementById("chartProcesoInv");
@@ -29,15 +29,35 @@ function chartProcesoInv() {
                 borderColor: '#F1C40F',
                 borderWidth: 2,
                 fill: true,
-                tension: 0.3
+                tension: 0.4
             }]
         },
         options: {
+            plugins: {
+                //formatea los tooltips de la grafica
+                tooltip: {
+                    callbacks: {
+                        title: (context) => {
+                            return context[0].label.replaceAll(',', ' ');
+                        }
+                    },
+                },
+                legend: {
+                    labels: {
+                        // This more specific font property overrides the global property
+                        font: {
+                            size: 14
+                        },
+                    }
+                }
+            },
             scales: {
-                x:{
-
+                x: {
+                    align: 'center',
                 },
                 y: {
+                    //Quita los dlabels del eje x
+                    display: false,
                     beginAtZero: true,
                     grid: {
                         display: false
